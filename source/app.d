74ba07c7dfb33c984f0c3ebfb7d0750dcd0070f0
TiberiuGal <galtiberiu@gmail.com>
2014-03-18 12:49:23 +0200

    updated MemcachedCluster MemcachedCluster is just a special case of memcachedClient

--- source/app.d
+++ source/app.d
@@ -4,19 +4,58 @@ import memcached4d;
 import vibe.d;
 void main(string[] args)
 {
-	Test t;
-	auto d = new MemcachedClient("192.168.1.215");
-	d.store("geta1", "polonik mare");
-	d.store("p1", t);
-	
-	writeln( d.get!string("geta1"));
-	writeln( d.get!Test("p1"));
+	// connect to a memcached server
 
+	auto d = memcachedConnect("192.168.1.215, 192.168.1.215, 192.168.1.215");
+
+	/* 
+	 // connect to a cluster 
+	MemcachedServer s;
+	s.host = "192.168.1.215";
+
+	MemcachedServers servers;
+	servers ~= s;
+
+	auto d = new MemcachedClusterClient(servers);
+	*/
+
+	// store a string;
+	d.store("geta1", "Hello World!");
+	// get back the stored string
+	writeln( d.get!string("geta1"));
+
+	//append data to a stored string
+	d.append("geta1", " from memcached");
+	writeln( d.get!string("geta1"));
+
+
+
+
+	// store and int
 	d.store("int_value", 2);
+	// increment
 	d.increment("int_value", 4);
 	assert( d.get!int("int_value") == 6, "int value incremented");
+	//decrement
 	d.decr("int_value", 3);
-	assert(d.get!int("int_value") == 2, "int value decremented" );
+	assert(d.get!int("int_value") == 3, "int value decremented" );
+
+
+	Test t;
+	// store a User defined type
+	d.store("p1", t);
+	// get back the stored data
+	writeln( d.get!Test("p1"));
+
+	int casId;
+	//get back a casID
+	auto p2 = d.gets!Test("p1", casId);
+	writefln("casId for p1 %s", casId); 
+
+	p2.a = 4;
+	// update an object if not modified since the last query
+	d.cas("p1", p2, casId);
+	writeln( d.get!Test("p1"));
 
 }
 
--- source/memcached4d.d
+++ source/memcached4d.d
@@ -1,11 +1,43 @@
 ﻿module memcached4d;
 
-// TODO: memcached cluster
+// TODO: 
 //	documentation
 // 	unit tests
-//  add expiration info
-//  error handling and reporting
-//  optionally implement all commands
+//  
+
+
+/**
+Memcached client for the D programming language.
+memcached is a distributed caching system (http://www.memcached.org)
+
+The basic idea is this: if you need to share/cache objects between applications you dump them into memcache in a serialized form.
+the data can be read back from other programs - even from other programming language - provided that the reader knows how to deserialize the data.
+a common way to serialize data is json. Memcached4d uses vibe serialization library to dump your data to json, but you can provide your own serialization method 
+by implementing a JSON toJson method in your objects.
+
+
+
+A similar tool - with a lot more features is Redis - you may have a look at vibe.db.redis
+
+
+usage
+	auto cache = memcachedConnect('127.0.0.1');
+	auto cache = memcachedConnect('127.0.0.1:11211');
+	auto cache = memcachedConnect( ['127.0.0.1', '127.0.0.1'] ); // you can connect the the same server multiplie times 
+	auto cache = memcachedConnect( '127.0.0.1, 127.0.0.1' ); 
+	auto cache = memcachedConnect( '127.0.0.1:11211, 127.0.0.1:11212' ); 
+	
+
+
+   if( cache.store("str_var", "lorem ipsum") == RETURN_STATE.SUCCESS ) {
+		writeln("stored successfully");
+		writeln( " get back the stored data : {", cache.get!string("str_var") , "}" );
+   }else {
+   		writeln("not stored")
+	}
+
+
+*/
 
 
 import std.stdio;
@@ -25,6 +57,16 @@ import vibe.core.net,
 struct MemcachedServer {
 	string host;
 	ushort port = 11211;
+	TCPConnection conn;
+	this(string hostString){
+		if( hostString.indexOf(':') != -1){
+			auto parts = split(hostString, ':');
+			this.host = strip(parts[0]);
+			this.port = strip(parts[1]).to!ushort;
+		} else {
+			this.host = strip(hostString);
+		}
+	}
 }
 
 alias MemcachedServer[] MemcachedServers;
@@ -32,73 +74,41 @@ alias MemcachedServer[] MemcachedServers;
 
 /**
  * Use this to connectect to a memcached cluster
- * this is confusing, but ...
- * a MemcachedCluster is a a client to a cluser of server
- * it holds a collection of clients ( each client connects to one server )
- * so client and server here are used interchangeably - but they mean the same thing.
+ *
  * 
 */
-class MemcachedCluster {
+class MemcachedClusterClient : MemcachedClient {
 
 
-	protected MemcachedClient[] clients;
+	protected  MemcachedServers servers;
 
 	this(MemcachedServers servers) {
-		foreach(server; servers){
-			clients ~= new MemcachedClient(server);
+		this.servers = servers;
+	}
+
+
+	override void connect(string key) {
+		auto server  = getServer(key);
+		this.conn = server.conn;
+		if (!conn || !conn.connected) {
+			try conn = connectTCP(server.host, server.port);
+			catch (Exception e) {
+				throw new Exception(format("Failed to connect to memcached server at %s:%s.", server.host, server.port), __FILE__, __LINE__, e);
+			}
+		}
+	}
+
+	override void disconnect() {
+		foreach(server; servers) {
+			if (server.conn && server.conn.connected)
+				server.conn.close();
 		}
 	}
 
 
 
-	/**
-	 *  store the value with one of the server
-	 * 
-	 *  compute hash
-	 *  determine server 
-	 *	connect client to server
-	 *	run client.store
-	*/
-	void store(T)(string key, T data, int expires = 0){
-		return getServer.get(key, data, expires);
-	}
-
-
-	/**
-	 * 	get the value form one of the servers
-	 * 
-	 *  compute hash
-	 *  determine server 
-	 *	connect client to server
-	 *	return client.get
-	*/
-	T get(T)(string key) {
-		return getServer.get(key);
-	}
-
-	void remove(string key, int time = 0){
-		getServer(key).remove(key, time);
-	}
-	alias remove del;
-	
-	void increment(string key, int inc){
-		getServer(key).increment(key, inc);
-	}
-	alias increment incr;	
-	
-	void decrement(string key, int dec){
-		getServer(key).decrement(key, dec);
-	}
-	alias decrement decr;	
-	
-	void touch(string key, int expires){
-		getServer(key).touch(key, expires); 
-	}
-
-
-
-	MemcachedClient getServer(string key) {
-		return   clients[ determineServer(key) ];
+	MemcachedServer getServer(string key) {
+		return servers[ determineServer(key) ];
 	}
 
 	int determineServer(string key){
@@ -112,10 +122,12 @@ class MemcachedCluster {
 	 * get the modulo of that last byte to the length of servers
 	 */ 
 	int equalWeights(string key){
-		return md5Of(key)[$-1] % clients.length;
+		return md5Of(key)[$-1] % servers.length;
 	}
 
 }
+
+enum RETURN_STATE {  SUCCESS, ERROR, STORED, EXISTS, NOT_FOUND, NOT_STORED };
 
 /**
  * Use this to connect and query a memcached server
@@ -126,19 +138,21 @@ class MemcachedClient {
 	TCPConnection conn;
 	protected MemcachedServer server;
 
+	this() {} // allow adding a different constructor in subclasses
 	
 	this(string host, ushort port = 11211) {
 		server.host = host;
 		server.port = port;
+		conn = server.conn;
 	}
 
 	this(MemcachedServer server){
 		this.server = server;
+		conn = server.conn;
 	}
 
 
-
-	void connect() {
+	void connect(string key) {
 		if (!conn || !conn.connected) {
 			try conn = connectTCP(server.host, server.port);
 			catch (Exception e) {
@@ -147,20 +161,22 @@ class MemcachedClient {
 		}
 	}
 
+	void disconnect() {
+		if( conn && conn.connected ){
+			conn.close();
+		}		
+	}
 
 
 	/**
-	 * store a data 
-	 * 
+	 * convert User data type to a string representation
 	 */
-	void store(T)(string key, T data, int expires = 0) {
-		connect();
-
+	protected string serialize(T)(T data) {
 		alias Unqual!T Unqualified;
-
+		
 		string value;
 		static if (is(Unqualified : string)) {
-			 value = data;
+			value = data;
 		} else static if(is(Unqualified : int) 
 		                 || is(Unqualified == bool)
 		                 || is(Unqualified == double)
@@ -172,50 +188,172 @@ class MemcachedClient {
 		else static if( isJsonSerializable!Unqualified ){
 			value = data.toJson;
 		} else  {
-			value = serialize!JsonSerializer(data).toString;
+			value = vibe.data.serialization.serialize!JsonSerializer(data).toString;
 		}
-		conn.write( format("set %s 0 %s %s\r\n%s\r\n", key, expires, value.length.to!string, value) );
-		
+		return value;
+	}
+
+	/**
+	 * store data with key, make it expire after expires secconds
+	 * if expires == 0 - data will not expire
+	 * 
+	 */
+	RETURN_STATE store(T)(string key, T data, int expires = 0) {
+		connect(key);
+		string value = serialize(data);
+		conn.write( format("set %s 0 %s %s\r\n%s\r\n", key, expires, value.length.to!string, value) );		
 		auto retval = cast(string) conn.readLine();
-		//if(retval == "STORED") ;
-		//if(retval == "ERROR") ;
+
+		if(retval == "STORED") { return RETURN_STATE.SUCCESS ; }
+		else { return RETURN_STATE.ERROR; }
 	}
 
 
 
-	void remove(string key, int time = 0){
-		connect();
+	RETURN_STATE add(T)(string key, T data, int expires = 0){
+		connect(key);
+		string value = serialize(data);
+		conn.write( format("add %s 0 %s %s\r\n%s\r\n", key, expires, value.length.to!string, value) );
+
+		auto retval = cast(string) conn.readLine();
+
+		if(retval == "STORED") { return RETURN_STATE.SUCCESS ; }
+		else if(retval == "NOT_STORED") { return RETURN_STATE.NOT_STORED ; }
+		else { return RETURN_STATE.ERROR; }
+	}
+
+	RETURN_STATE replace(T)(string key, T data, int expires = 0){
+		connect(key);
+		string value = serialize(data);
+		conn.write( format("replace %s 0 %s %s\r\n%s\r\n", key, expires, value.length.to!string, value) );
+
+		auto retval = cast(string) conn.readLine();
+
+		if(retval == "STORED") { return RETURN_STATE.SUCCESS ; }
+		else if(retval == "NOT_STORED") { return RETURN_STATE.NOT_STORED ; }
+		else { return RETURN_STATE.ERROR; }
+	}
+
+
+	RETURN_STATE append(T)(string key, T data){
+		connect(key);
+		string value = serialize(data);
+		conn.write( format("append %s 0 0 %s\r\n%s\r\n", key, value.length.to!string, value) );
+
+		auto retval = cast(string) conn.readLine();
+		if(retval == "STORED") { return RETURN_STATE.SUCCESS ; }
+		else if(retval == "NOT_STORED") { return RETURN_STATE.NOT_STORED ; }
+		else { return RETURN_STATE.ERROR; }
+	}
+
+	RETURN_STATE prepend(T)(string key, T data){
+		connect(key);
+		string value = serialize(data);
+		conn.write( format("prepend %s 0 0 %s\r\n%s\r\n", key, value.length.to!string, value) );
+
+		auto retval = cast(string) conn.readLine();
+		if(retval == "STORED") { return RETURN_STATE.SUCCESS ; }
+		else if(retval == "NOT_STORED") { return RETURN_STATE.NOT_STORED ; }
+		else { return RETURN_STATE.ERROR; }
+	}
+
+
+	RETURN_STATE cas(T)(string key, T data, int casId, int expires = 0){
+		connect(key);
+		string value = serialize(data);
+		conn.write( format("cas %s 0 %s %s %s\r\n%s\r\n", key, expires, value.length.to!string, casId, value) );
+
+		auto retval = cast(string) conn.readLine();
+		if(retval == "STORED") { return RETURN_STATE.SUCCESS ; }
+		else if(retval == "NOT_FOUND") { return RETURN_STATE.NOT_FOUND ; }
+		else if(retval == "EXISTS") { return RETURN_STATE.EXISTS ; }
+		else { return RETURN_STATE.ERROR; }
+	}
+
+
+	RETURN_STATE remove(string key, int time = 0){
+		connect(key);
 		conn.write( format("delete %s %s\r\n", key, time));
-		conn.readLine;
+
+		auto retval = cast(string) conn.readLine();
+		if(retval == "DELETED") { return RETURN_STATE.SUCCESS ; }
+		else if(retval == "NOT_FOUND") { return RETURN_STATE.NOT_FOUND ; }
+		else { return RETURN_STATE.ERROR; }
 	}
 	alias remove del;
 
-	void increment(string key, int inc){
-		connect();
+	RETURN_STATE increment(string key, int inc){
+		connect(key);
 		conn.write( format("incr %s %s\r\n", key, inc));
-		conn.readLine;
+
+		auto retval = cast(string) conn.readLine();
+		if( retval.isNumeric() ) { return RETURN_STATE.SUCCESS; }
+		else if(retval == "NOT_FOUND") { return RETURN_STATE.NOT_FOUND ; }
+		else if(retval.startsWith("CLIENT_ERROR")) { return RETURN_STATE.ERROR ; }
+		else if(retval == "ERROR" ) { return RETURN_STATE.ERROR ; }
+		else { return RETURN_STATE.ERROR; }
+
 	}
 	alias increment incr;
 	
 	
-	void decrement(string key, int dec){
-		connect();
+	RETURN_STATE decrement(string key, int dec){
+		connect(key);
 		conn.write( format("decr %s %s\r\n", key, dec));
-		conn.readLine;
+
+		auto retval = cast(string) conn.readLine();
+		if( retval.isNumeric() ) { return RETURN_STATE.SUCCESS; }
+		else if(retval == "NOT_FOUND") { return RETURN_STATE.NOT_FOUND ; }
+		else if(retval.startsWith("CLIENT_ERROR")) { return RETURN_STATE.ERROR ; }
+		else if(retval == "ERROR" ) { return RETURN_STATE.ERROR ; }
+		else { return RETURN_STATE.SUCCESS; }
 	}
 	alias decrement decr;
 	
 	
-	void touch(string key, int expires){
-		connect();
+	RETURN_STATE touch(string key, int expires){
+		connect(key);
 		conn.write( format("touch %s %s\r\n", key, expires) );
-		conn.readLine();
+
+		auto retval = cast(string) conn.readLine();
+		if(retval == "TOUCHED") { return RETURN_STATE.SUCCESS ; }
+		else if(retval == "NOT_FOUND") { return RETURN_STATE.NOT_FOUND ; }
+		else { return RETURN_STATE.ERROR; }
 	}
 
 
-	T get(T)(string key){
-		connect();
+	/**
+	 * convert data stored in memcached to the requested type
+	 */ 
+	protected T deserialize(T)(string data) {
 		alias Unqual!T Unqualified;
+		
+		static if(is(Unqualified: string)){
+			return data;
+		}else static if(is(Unqualified : int) 
+		                || is(Unqualified == bool)
+		                || is(Unqualified == double)
+		                || is(Unqualified == float)
+		                || is(Unqualified : long)
+		                ){
+			
+			return chomp(data).to!T;
+		} else {
+			Json j = parseJsonString( data);
+			return deserializeJson!T(j);
+		}
+	}
+
+
+
+	/**
+	 *  get back data from the memcached server,
+	 *  return it as type T
+	 *  
+	 * if the key is not found, it will return an empty string
+	 */
+	T get(T)(string key){
+		connect(key);
 		conn.write( std.string.format("get %s \r\n", key ) );
 		auto tmp = std.array.appender!string;
 		do{
@@ -224,32 +362,69 @@ class MemcachedClient {
 				continue;
 			else if(ln == "END") 
 				break;
-
+			
 			tmp.put(ln ~ "\r\n");
-
+			
 		} while( true); 
 
-		//writefln("dumping data for key %s = {%s}", key, tmp.data);
-
-		static if(is(Unqualified: string)){
-			return tmp.data;
-		}else static if(is(Unqualified : int) 
-		                || is(Unqualified == bool)
-		                || is(Unqualified == double)
-		                || is(Unqualified == float)
-		                || is(Unqualified : long)
-		                ){
-
-			return chomp(tmp.data).to!T;
-		} else {
-			Json j = parseJsonString( tmp.data);
-			return deserializeJson!T(j);
-		}
+		return deserialize!T( tmp.data );
+	}
 
 
+	/**
+	 * get back data form the memcached server,
+	 * return it as type T,
+	 * 
+	 * populate the casId variable with the cas_unique_id 
+	 * 
+	 */ 
+	T gets(T)(string key, out int casId) {
+		connect(key);
+		conn.write( std.string.format("gets %s \r\n", key ) );
+
+		auto tmp = std.array.appender!string;
+		do{
+			string ln = cast(string) conn.readLine();
+			if(ln.startsWith("VALUE " ~ key)) {
+				auto parts = split(ln);
+				casId = parts[ $-1 ].to!int;
+				continue;
+			} else if(ln == "END") {
+				break;
+			}
+			
+			tmp.put(ln ~ "\r\n");
+			
+		} while( true); 
+		
+		return deserialize!T( tmp.data );
 	}
 	
 }
 
+/**
+ * connect to a memcached server or server cluster
+ * 
+ * 
+ */ 
+MemcachedClient memcachedConnect(string hostString){
+	if( hostString.indexOf(',') >=0 ) {
+		auto hosts = split(hostString, ',');
+		return memcachedConnect( hosts);
+	}
+	return new MemcachedClient( MemcachedServer(hostString) );
+}
 
+/**
+ * connect to a memcached cluster
+ * 
+ * 
+ */ 
+MemcachedClusterClient memcachedConnect( string[] hostStrings ){
+	MemcachedServers servers;
+	foreach(host; hostStrings){
+		servers ~= MemcachedServer(host);
+	}
+	return new MemcachedClusterClient(servers);
+}
 
